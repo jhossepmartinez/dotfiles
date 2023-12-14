185747defa630e53cf56a54ae861e050a8caecfb
jhossepmartinez <jhossepmv@gmail.com>
2023-12-14 18:15:08 -0300

    added rest plugin to test

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -11,6 +11,28 @@ return {
             })
         end
     },
+    {
+        "rest-nvim/rest.nvim",
+        config = function()
+            require("rest-nvim").setup({
+                result_split_horizontal = false,
+                skip_ssl_verification = false,
+                highlight = {
+                    enabled = true,
+                    timeout = 150
+                },
+                result = {
+                    show_url = true,
+                    show_http_indo = true,
+                    show_headers = true,
+                },
+                jump_to_request = false,
+                env_file = ".env",
+                custom_dynamic_variables = {},
+                yank_dry_run = true
+            })
+        end
+    },
     -- {
     --     "roobert/tailwindcss-colorizer-cmp.nvim",
     --     -- optionally, override the default options:
