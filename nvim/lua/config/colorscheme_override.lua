573344dc60bacc9ff6aad17f5b37fcb01a10f104
jhossepmartinez <jhossepmv@gmail.com>
2024-01-04 00:09:39 -0300

    added some transparent overrides

--- nvim/lua/config/colorscheme_override.lua
+++ nvim/lua/config/colorscheme_override.lua
@@ -1,19 +1,21 @@
 local M = {}
 function M.transparent_override()
     local highlights = {
-        "Normal",
-        "NormalNC",
-        "LineNr",
+        -- "Normal",
+        -- "NormalNC",
+        -- "LineNr",
         -- "LineNr", "Folded", "NonText",
         -- "SpecialKey", "VertSplit",
         -- "EndOfBuffer", 'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier', 'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function', 'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText', 'SignColumn', 'CursorLineNr', 'EndOfBuffer', 'MsgArea', 'CursorLine', 
-        "SignColumn", 
+        "SignColumn",
 
         -- Neotree highlights
-        "NeoTreeCursorLine",
-        "NeoTreeNormal",
-        "NeoTreeNormalNC",
-        "NeoTreeWinSeparator",
+        -- "NeoTreeCursorLine",
+        -- "NeoTreeNormal",
+        -- "NeoTreeNormalNC",
+        -- "NeoTreeWinSeparator",
+        "WinSeparator",
+
     }
     for _, highlight in pairs(highlights) do
         vim.cmd('hi ' .. highlight .. ' guibg=none ctermbg=none')
@@ -25,7 +27,14 @@ function M.colorscheme_addon()
     -- vim.cmd("hi Structure guifg=#cdcfe4")
     -- vim.cmd("hi Normal guifg=#98a2b0")
     vim.cmd("hi Comment guifg=#697281")
+    vim.cmd("hi LineNr guifg=#c0c8d8")
     -- vim.cmd("hi Statement guifg=#9c9fc9")
+end
+
+function M.colorscheme_default()
+    vim.cmd("hi SignColumn guibg=none ctermbg=none")
+    vim.cmd("hi CursorLine cterm=none")
+    vim.cmd("hi Comment guifg=#697281")
 end
 
 return M
