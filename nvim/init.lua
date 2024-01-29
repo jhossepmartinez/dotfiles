d1f26c090a660af8f24fa8e58024b3ff896e4039
jhossepmartinez <jhossepmv@gmail.com>
2024-01-29 10:32:35 -0300

    fixed forma

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,14 @@
 -- Lazy plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -22,7 +22,7 @@ require("lazy").setup("plugins")
 
 -- Set Colorscheme
 -- vim.g.ayucolor='mirage'
-vim.cmd("colorscheme nordic") -- colorscheme before configuration so it can be overriden by preferences
+vim.cmd("colorscheme rose-pine-moon")
 -- Nisha theme seems nice
 
 -- Transparency override
