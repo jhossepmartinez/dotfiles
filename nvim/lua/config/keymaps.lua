0e84b6b99273dad839cb39594083df750024f51c
jhossepmartinez <jhossepmv@gmail.com>
2024-01-29 10:31:14 -0300

    modified cursor movement to allow movement in virtual lines (wrapped)

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,12 +7,13 @@ keymap("i", "jk", "<ESC>")
 
 -- Split window management
 keymap("n", "<Leader>cs", ":close<CR>")
-
 keymap("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
 keymap("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
 keymap("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
 keymap("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
 keymap("n", "<Leader>q", ":bp<bar>sp<bar>bn<bar>bd<CR>", { desc = "Close current buffer"})
+keymap("n", "j", "gj", { noremap = true, silent = true })
+keymap("n", "k", "gk", { noremap = true, silent = true })
 -- keymap("n", "zz", "<cmd>w<CR>", { desc = "Save current buffer" })
 
 -- Global diagnostics
