c0011aedab5af69a45c9bc55e3ae9db03015c35f
jhossepmartinez <jhossepmv@gmail.com>
2024-02-07 15:42:29 -0300

    config modified treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,31 +1,32 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "c",
-                    "lua",
-                    "vim",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"c",
+					"lua",
+					"vim",
 
-                    "markdown",
+					"markdown",
 
-                    "rust",
+					"rust",
 
-                    "javascript",
-                    "html"
-                },
-                highlight = {
-                    enable = true,
-                    disable = {
-                        "markdown"
-                    }
-                    -- additional_vim_regex_highlighting = true
-                },
-                indent = {
-                    enable = true
-                }
-            })
-        end
-    }
+					"javascript",
+					"html",
+				},
+				highlight = {
+					enable = true,
+					disable = {
+						"markdown",
+						"lua",
+					},
+					-- additional_vim_regex_highlighting = true
+				},
+				indent = {
+					enable = true,
+				},
+			})
+		end,
+	},
 }
