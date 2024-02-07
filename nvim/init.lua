a751cf6675c1961387317d9a2cb4757db3449abd
jhossepmartinez <jhossepmv@gmail.com>
2024-02-07 15:44:06 -0300

    changed colorscheme used

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,14 @@
 -- Lazy plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,13 +21,12 @@ vim.g.mapleader = " " -- mapleader must be loaded before lazy
 require("lazy").setup("plugins")
 
 -- Set Colorscheme
--- vim.g.ayucolor='mirage'
-vim.cmd("colorscheme rose-pine-moon")
+-- vim.g.ayucolor = "mirage"
+vim.cmd("colorscheme embark")
 -- Nisha theme seems nice
 
 -- Transparency override
 local theme = require("config.colorscheme_override")
 theme.colorscheme_addon()
 theme.transparent_override()
--- theme.colorscheme_default()
-
+theme.colorscheme_default()
