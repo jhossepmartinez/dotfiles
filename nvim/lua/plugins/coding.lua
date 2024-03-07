cd70189950abbfa86320f81aec8b20d67afefdf4
jhossepmv <jhossepmv@gmail.com>
2024-03-07 19:15:41 -0300

    modified some colorschmes

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -1,69 +1,77 @@
 return {
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        config = function()
-            local highlights = {
-                "IndentEven",
-                "IndentOdd",
-            }
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		config = function()
+			local highlights = {
+				"IndentEven",
+				"IndentOdd",
+			}
 
-            local hooks = require("ibl.hooks")
+			local hooks = require("ibl.hooks")
 
-            hooks.register(hooks.type.HIGHLIGHT_SETUP,function()
-                vim.api.nvim_set_hl(0, "IndentEven", { fg = "#8893a7"})
-                vim.api.nvim_set_hl(0, "IndentOdd", { fg = "#8893a7"})
-            end)
+			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+				vim.api.nvim_set_hl(0, "IndentEven", { fg = "#5e6674" })
+				vim.api.nvim_set_hl(0, "IndentOdd", { fg = "#5e6674" })
+			end)
 
-            require("ibl").setup({
-                indent = { char = "▏", highlight = highlights },
-                scope = {
-                    enabled = false
-                },
-                exclude = {
-                    filetypes = {
-                        -- "markdown"
-                    }
-                }
-            })
-        end,
-    },
-    {
-        "kevinhwang91/nvim-ufo",
-        dependencies = {
-            "kevinhwang91/promise-async",
-            "luukvbaal/statuscol.nvim"
-        },
-        config = function()
-            require("ufo").setup({
-                provider_selector = function(bufnr, filetype, buftype)
-                    return { "lsp", "indent" }
-                end
-            })
+			require("ibl").setup({
+				indent = { char = "▏", highlight = highlights },
+				scope = {
+					enabled = false,
+				},
+				exclude = {
+					filetypes = {
+						-- "markdown"
+					},
+				},
+			})
+		end,
+	},
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = {
+			"kevinhwang91/promise-async",
+			"luukvbaal/statuscol.nvim",
+		},
+		config = function()
+			require("ufo").setup({
+				provider_selector = function(bufnr, filetype, buftype)
+					return { "lsp", "indent" }
+				end,
+			})
 
-            vim.o.foldcolumn = '1' -- '0' is not bad
-            vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-            vim.o.foldlevelstart = 99
-            vim.o.foldenable = true
+			vim.o.foldcolumn = "1" -- '0' is not bad
+			vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+			vim.o.foldlevelstart = 99
+			vim.o.foldenable = true
 
-            -- Remove depth number indicator
-            local builtin = require("statuscol.builtin")
-            require("statuscol").setup(
-                {
-                    relculright = true,
-                    segments = {
-                        {text = { "%s", "" }, click = "v:lua.ScSa" },
-                        {text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
-                        {text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
-                    }
-                }
-            )
-        end,
-    },
-    {
-        "kdheepak/lazygit.nvim",
-        keys = {
-            { "<Leader>gg", "<cmd>LazyGit<CR>" }
-        }
-    }
+			-- Remove depth number indicator
+			local builtin = require("statuscol.builtin")
+			require("statuscol").setup({
+				relculright = true,
+				segments = {
+					{ text = { "%s", "" }, click = "v:lua.ScSa" },
+					{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+					{ text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
+				},
+			})
+		end,
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		keys = {
+			{ "<Leader>gg", "<cmd>LazyGit<CR>" },
+		},
+	},
+	{ "junegunn/fzf", build = "./install --bin" },
+	{
+		"ibhagwan/fzf-lua",
+		-- optional for icon support
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			-- calling `setup` is optional for customization
+			-- require("fzf-lua").setup()
+		end,
+	},
 }
--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -2,5 +2,8 @@ return {
 	{
 		"embark-theme/vim",
 		name = "embark",
+		config = function()
+			vim.g.embark_terminal_italics = 1
+		end,
 	},
 }
