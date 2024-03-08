d291f60b300813864052b3d4f60e3206ae814b57
jhossepmv <jhossepmv@gmail.com>
2024-03-07 21:25:43 -0300

    changed color / changed some comment

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,8 @@ end
 
 # Minimal fish prompt
 function fish_prompt
-    set -g __fish_git_prompt_color_branch "#f05d21" -i # -i Sets italics mode. and uses bright from privded color scheme 
+    # set -g __fish_git_prompt_color_branch "#f05d21" -i # -i Sets italics mode. and uses bright from privded color scheme 
+    set -g __fish_git_prompt_color_branch "#058f78" -i # -i Sets italics mode. and uses bright from privded color scheme 
     set -l textcol "#f2f2f2"
 
     # Fix to make prompt appear at first line
@@ -23,13 +24,10 @@ function fish_prompt
     # Git information prompt
     echo -n (fish_git_prompt | tr -d "() ")" "
 
-    # Node version information prompt
+    # Node version prompt
     fish_prompt_node_version
 
     # Virtual environment prompt
-    function __fish_virtualenv_prompt; 
-    end
-
     fish_prompt_env
 
     set_color normal -o
@@ -52,6 +50,7 @@ alias eldf="exa -lh --icons -s type"
 # Environment variables
 export VIRTUAL_ENV_DISABLE_PROMPT=1 # Disables virtual env prompt
 export PATH="$HOME/.cargo/bin:$PATH" # Patch to run cargo with fish shell
+export JAVA_HOME="/usr/lib/jvm/java-1.11.0-openjdk-amd64"
 
 # Node fisher plugin default version (jorgebucaran/nvm.fish)
 set --universal nvm_default_version v18.19.0
