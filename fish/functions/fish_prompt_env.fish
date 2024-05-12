202e69076c27de937f8b67365d17968d1f444274
jhossepForcast <jhossep.martinez@forcast.tech>
2024-05-11 23:50:04 -0400

    Changed color of venv prompt

--- fish/functions/fish_prompt_env.fish
+++ fish/functions/fish_prompt_env.fish
@@ -1,4 +1,5 @@
 function fish_prompt_env
+    set_color normal -o
     if test -n "$VIRTUAL_ENV" # Checks if VIRTUAL_ENV variable is set
         echo -n "🐍 "
         echo -n (basename $VIRTUAL_ENV | tr -d "()")" "
