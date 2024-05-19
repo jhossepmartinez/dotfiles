e5c25ef7960dfa6febd19166931a7bf02c6ef442
jhossepmartinez <jhossepmv@gmail.com>
2024-05-19 19:37:26 -0400

    Added back some cool colorschemes

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -6,4 +6,21 @@ return {
 			vim.g.embark_terminal_italics = 1
 		end,
 	},
+	{
+		"AlexvZyl/nordic.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("nordic").load()
+		end,
+	},
+	{
+		"Shatur/neovim-ayu",
+		config = function()
+			require("ayu").setup({
+
+				mirage = true,
+			})
+		end,
+	},
 }
