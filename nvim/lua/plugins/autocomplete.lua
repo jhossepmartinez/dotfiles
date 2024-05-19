f13ae4243617b8098a0088d061942eaac3f93fb1
jhossepmartinez <jhossepmv@gmail.com>
2024-05-19 19:36:13 -0400

    Fixed indentation

--- nvim/lua/plugins/autocomplete.lua
+++ nvim/lua/plugins/autocomplete.lua
@@ -1,45 +1,45 @@
 return {
-    { "onsails/lspkind.nvim" },
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "hrsh7th/cmp-buffer" },
-    { "hrsh7th/cmp-path" },
-    { "hrsh7th/cmp-cmdline" },
-    {
-        "L3MON4D3/LuaSnip",
-        dependencies = { "rafamadriz/friendly-snippets" }
-    },
-    { "saadparwaiz1/cmp_luasnip" },
-    {
-        "hrsh7th/nvim-cmp",
-        config = function()
-            local lspkind = require("lspkind")
-            local cmp = require("cmp")
-            require("luasnip.loaders.from_vscode").lazy_load()
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ["<Tab>"] = cmp.mapping.select_next_item(),
-                    ["<CR>"] = cmp.mapping.confirm(),
-                    -- ["<C-Space>"] = cmp.mapping.complete(),
-                }),
-                sources = cmp.config.sources({
-                    { name = 'luasnip' }, -- For vsnip users.
-                    { name = 'buffer' },
-                    { name = 'path' },
-                    { name = 'nvim_lsp' },
-                }),
-                formatting = {
-                    format = lspkind.cmp_format({
-                        mode = "symbol",
-                        maxwidth = 50,
-                        ellipsis_char = "...", -- filler char if popup exceeds maxwidth
-                    })
-                }
-            })
-        end
-    }
+	{ "onsails/lspkind.nvim" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "hrsh7th/cmp-cmdline" },
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = { "rafamadriz/friendly-snippets" },
+	},
+	{ "saadparwaiz1/cmp_luasnip" },
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			local lspkind = require("lspkind")
+			local cmp = require("cmp")
+			require("luasnip.loaders.from_vscode").lazy_load()
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<Tab>"] = cmp.mapping.select_next_item(),
+					["<CR>"] = cmp.mapping.confirm(),
+					-- ["<C-Space>"] = cmp.mapping.complete(),
+				}),
+				sources = cmp.config.sources({
+					{ name = "luasnip" }, -- For vsnip users.
+					{ name = "buffer" },
+					{ name = "path" },
+					{ name = "nvim_lsp" },
+				}),
+				formatting = {
+					format = lspkind.cmp_format({
+						mode = "symbol",
+						maxwidth = 50,
+						ellipsis_char = "...", -- filler char if popup exceeds maxwidth
+					}),
+				},
+			})
+		end,
+	},
 }
