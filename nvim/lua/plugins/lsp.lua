f3a70f96a693b68cc3d0d85d61a5b3f13133022b
jhossepmartinez <jhossepmv@gmail.com>
2024-05-19 19:35:49 -0400

    Indentation fix

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,60 +1,61 @@
 -- Mason lspconfig names
 local lsp_servers = {
-    "lua_ls",
-    "vimls",
+	"lua_ls",
+	"vimls",
 
-    "pyright",
-    "clangd",
-    "rust_analyzer",
+	"pyright",
+	"clangd",
+	"rust_analyzer",
 
-    "tsserver",
-    "html",
-    "cssls",
-    "tailwindcss",
-    "emmet_language_server",
+	"tsserver",
+	"html",
+	"cssls",
+	"tailwindcss",
+	"emmet_language_server",
+	"angularls",
 }
 
 return {
-    {
-        "williamboman/mason.nvim"
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason").setup()
-            require("mason-lspconfig").setup({
-                ensure_installed = lsp_servers
-            })
-            -- Automatic setup for lsp servers
-            require("mason-lspconfig").setup_handlers({
-                -- Called for each installed server
-                function(server_name)
-                    local capabilities = require("cmp_nvim_lsp").default_capabilities()
-                    require("lspconfig")[server_name].setup({ capabilities = capabilities })
-                end,
-                -- Specific configuration for each server
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup({
-                        settings = {
-                            Lua = {
-                                diagnostics = {
-                                    globals = { "vim" }
-                                }
-                            }
-                        }
-                    })
-                end
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            -- Diable / Enable inline diagnostics
-            vim.diagnostic.config({
-                virtual_text = true
-            })
-        end
-    }
+	{
+		"williamboman/mason.nvim",
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = lsp_servers,
+			})
+			-- Automatic setup for lsp servers
+			require("mason-lspconfig").setup_handlers({
+				-- Called for each installed server
+				function(server_name)
+					local capabilities = require("cmp_nvim_lsp").default_capabilities()
+					require("lspconfig")[server_name].setup({ capabilities = capabilities })
+				end,
+				-- Specific configuration for each server
+				["lua_ls"] = function()
+					local lspconfig = require("lspconfig")
+					lspconfig.lua_ls.setup({
+						settings = {
+							Lua = {
+								diagnostics = {
+									globals = { "vim" },
+								},
+							},
+						},
+					})
+				end,
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			-- Diable / Enable inline diagnostics
+			vim.diagnostic.config({
+				virtual_text = true,
+			})
+		end,
+	},
 }
