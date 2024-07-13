941abeeaff424593ba1c5a0e119353cd7cd2a99f
jhossepmartinez <jhossepmv@gmail.com>
2024-07-13 17:56:45 -0400

    Change to format_after_save
    works better in wsl

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -20,29 +20,27 @@ return {
 					typescriptreact = { "prettier" },
 					scss = { "prettier" },
 					json = { "prettier" },
-					-- jsonc = { "prettier" },
+					jsonc = { "prettier" },
 					html = { "prettier" },
 					python = { "black" },
 					-- ckl = { "prettier" },
 				},
-				-- format_on_save = {
-				-- These options will be passed to conform.format()
-				-- timeout_ms = 500,
-				-- lsp_format = "fallback",
-				-- },
+				format_after_save = {
+					lsp_format = "fallback",
+				},
 			})
 
-			-- Format on save
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				pattern = "*",
-				callback = function(args)
-					local cwd = vim.fn.getcwd()
-					-- Only format if not in work/ directory
-					-- if not string.match(cwd, "/work/") then
-						require("conform").format({ async = true, bufnr = args.buf })
-					-- end
-				end,
-			})
+			-- -- Format on save
+			-- vim.api.nvim_create_autocmd("BufWritePre", {
+			-- 	pattern = "*",
+			-- 	callback = function(args)
+			-- 		local cwd = vim.fn.getcwd()
+			-- 		-- Only format if not in work/ directory
+			-- 		-- if not string.match(cwd, "/work/") then
+			-- 		require("conform").format({ async = true, bufnr = args.buf })
+			-- 		-- end
+			-- 	end,
+			-- })
 		end,
 	},
 	{
