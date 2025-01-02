4ee1241445d21cb5a1df4eb61218cce7796c09c0
jhossepmartinez <jhossepmv@gmail.com>
2025-01-02 20:20:36 -0300

    add supermaven

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -12,5 +12,17 @@ return {
 				},
 			})
 		end,
+		enabled = false,
+	},
+	{
+		"supermaven-inc/supermaven-nvim",
+		config = function()
+			require("supermaven-nvim").setup({
+				keymaps = {
+					accept_suggestion = "<Tab>",
+				},
+				log_level = "info",
+			})
+		end,
 	},
 }
