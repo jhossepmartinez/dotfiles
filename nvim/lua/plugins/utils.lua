19352e6f72c570a11db5ed0e8c524fdf7e191d9d
jhossepmartinez <jhossepmv@gmail.com>
2025-02-22 21:28:54 -0300

    switch colorizer for oklch

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -1,16 +1,33 @@
 return {
+	-- {
+	-- 	"norcalli/nvim-colorizer.lua",
+	-- 	config = function()
+	-- 		require("colorizer").setup({
+	-- 			"*",
+	-- 			css = {
+	-- 				rgb_fn = true,
+	-- 				hsl_fn = true,
+	-- 				names = true,
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 	{
-		"norcalli/nvim-colorizer.lua",
-		config = function()
-			require("colorizer").setup({
-				"*",
-				css = {
-					rgb_fn = true,
-					hsl_fn = true,
-					names = true,
-				},
-			})
-		end,
+		"eero-lehtinen/oklch-color-picker.nvim",
+		event = "VeryLazy",
+		version = "*",
+		keys = {
+			-- One handed keymap recommended, you will be using the mouse
+			{
+				"<leader>v",
+				function()
+					require("oklch-color-picker").pick_under_cursor()
+				end,
+				desc = "Color pick under cursor",
+			},
+		},
+		---@type oklch.Opts
+		opts = {},
 	},
 	{
 		"numToStr/Comment.nvim",
