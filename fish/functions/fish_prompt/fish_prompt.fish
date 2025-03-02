33c07092e087532178bc77d0add586574c22539c
jhossepmartinez <jhossepmv@gmail.com>
2025-03-02 00:08:16 -0300

    update fish prompt

--- fish/functions/fish_prompt/fish_prompt.fish
+++ fish/functions/fish_prompt/fish_prompt.fish
@@ -9,8 +9,9 @@ function fish_prompt
     set -l statusb_color (set_color --bold $fish_color_status)
     set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-    set -l newline_to_add (test "$history[1]" != "clear" -a "$history[1]" != "cl"; and echo "\n"; or echo "")
+    set -l newline_to_add (test "$history[1]" != "clear" -a "$history[1]" != "cl" -a "$first_prompt" = "true"; and echo "\n"; or echo "")
 
     echo -e -n -s $newline_to_add (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) " " $prompt_status "\n❯ "
+    set -g first_prompt true
 end
 
