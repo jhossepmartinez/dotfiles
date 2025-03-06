3e48744195845269e4df323a762a7fab61f57726
jhossepmartinez <jhossepmv@gmail.com>
2025-03-05 22:31:59 -0300

    add git aliases

--- fish/aliases/git.fish
+++ fish/aliases/git.fish
@@ -1,2 +1,6 @@
 alias gco="git checkout"
+alias gcb="git checkout -b"
 alias gpo="git pull origin"
+alias gb="git branch"
+# Pretty log
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
