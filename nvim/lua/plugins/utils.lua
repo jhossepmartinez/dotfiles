3bd018365a68050a7795af572ae329c3c0906753
jhossepmartinez <jhossepmv@gmail.com>
2025-04-05 20:22:23 -0300

    added some pretty hover plugins

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -44,4 +44,53 @@ return {
 		"windwp/nvim-autopairs",
 		config = true,
 	},
+	{
+		"lewis6991/hover.nvim",
+		config = function()
+			require("hover").setup({
+				init = function()
+					require("hover.providers.diagnostic")
+					require("hover.providers.lsp")
+				end,
+				preview_opts = {
+					border = "single",
+				},
+				-- Whether the contents of a currently open hover window should be moved
+				-- to a :h preview-window when pressing the hover keymap.
+				preview_window = false,
+				title = true,
+				mouse_providers = {
+					"LSP",
+				},
+				mouse_delay = 1000,
+			})
+			-- Setup keymaps
+			vim.keymap.set("n", "K", require("hover").hover, { desc = "hover.nvim" })
+			vim.keymap.set("n", "gK", require("hover").hover_select, { desc = "hover.nvim (select)" })
+			vim.keymap.set("n", "<C-p>", function()
+				require("hover").hover_switch("previous")
+			end, { desc = "hover.nvim (previous source)" })
+			vim.keymap.set("n", "<C-n>", function()
+				require("hover").hover_switch("next")
+			end, { desc = "hover.nvim (next source)" })
+
+			-- Mouse support
+			vim.keymap.set("n", "<MouseMove>", require("hover").hover_mouse, { desc = "hover.nvim (mouse)" })
+			vim.o.mousemoveevent = true
+		end,
+	},
+	-- {
+	-- 	"Fildo7525/pretty_hover",
+	-- 	event = "LspAttach",
+	-- 	opts = {},
+	-- 	keys = {
+	-- 		{
+	-- 			"<leader>p",
+	-- 			function()
+	-- 				require("pretty_hover").hover()
+	-- 			end,
+	-- 			desc = "Toggle pretty hover",
+	-- 		},
+	-- 	},
+	-- },
 }
