c5b9143291675215e63c18822e70ddd7f84c96c6
jhossepmartinez <jhossepmv@gmail.com>
2025-04-30 18:43:38 -0400

    added command separators

--- fish/functions/fish_prompt/fish_prompt.fish
+++ fish/functions/fish_prompt/fish_prompt.fish
@@ -10,11 +10,12 @@ function fish_prompt
     set -l statusb_color (set_color --bold $fish_color_status)
     set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
 
-    set -l newline_to_add (test "$history[1]" != "clear" -a "$history[1]" != "cl" -a "$first_prompt" = "true"; and echo "\n"; or echo "")
+    set -l separator_fill (string repeat -n (math $COLUMNS - 2) "─")
+    set -l separator_line (test "$history[1]" != "clear" -a "$history[1]" != "cl" -a "$first_prompt" = "true"; and echo "\n\033[2m$separator_fill\033[0m\n\n"; or echo "")
 
     set -l virtual_env_prompt (basename "$VIRTUAL_ENV")
 
-    echo -e -n -s $newline_to_add \
+    echo -e -n -s $separator_line \
         (set_color $color_cwd) (prompt_pwd) \
         (set_color normal) (fish_vcs_prompt) " " \
         (node_version_prompt_fragment) \
