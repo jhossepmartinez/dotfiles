4d47dd5b119788c06b4ada852ac640fbc3c85323
jhossepmartinez <jhossepmv@gmail.com>
2025-09-17 00:06:57 -0300

    remove comment

--- nvim/lsp/eslint.lua
+++ nvim/lsp/eslint.lua
@@ -54,41 +54,6 @@ return {
 		"bun.lock",
 		"deno.lock",
 	},
-	-- root_dir = function(bufnr, on_dir)
-	-- 	-- The project root is where the LSP can be started from
-	-- 	-- As stated in the documentation above, this LSP supports monorepos and simple projects.
-	-- 	-- We select then from the project root, which is identified by the presence of a package
-	-- 	-- manager lock file.
-	-- 	local root_markers =
-	-- 		{ "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "bun.lockb", "bun.lock", "deno.lock" }
-	-- 	-- Give the root markers equal priority by wrapping them in a table
-	-- 	root_markers = vim.fn.has("nvim-0.11.3") == 1 and { root_markers, { ".git" } }
-	-- 		or vim.list_extend(root_markers, { ".git" })
-	-- 	-- We fallback to the current working directory if no project root is found
-	-- 	local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
-	--
-	-- 	-- We know that the buffer is using ESLint if it has a config file
-	-- 	-- in its directory tree.
-	-- 	--
-	-- 	-- Eslint used to support package.json files as config files, but it doesn't anymore.
-	-- 	-- We keep this for backward compatibility.
-	-- 	local filename = vim.api.nvim_buf_get_name(bufnr)
-	-- 	local eslint_config_files_with_package_json =
-	-- 		util.insert_package_json(eslint_config_files, "eslintConfig", filename)
-	-- 	local is_buffer_using_eslint = vim.fs.find(eslint_config_files_with_package_json, {
-	-- 		path = filename,
-	-- 		type = "file",
-	-- 		limit = 1,
-	-- 		upward = true,
-	-- 		stop = vim.fs.dirname(project_root),
-	-- 	})[1]
-	-- 	if not is_buffer_using_eslint then
-	-- 		return
-	-- 	end
-	--
-	-- 	on_dir(project_root)
-	-- end,
-	-- Refer to https://github.com/Microsoft/vscode-eslint#settings-options for documentation.
 	settings = {
 		validate = "on",
 		packageManager = nil,
