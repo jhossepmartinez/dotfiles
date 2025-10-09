9d7aa5878cb25c56af7c65685887caa2e4329e98
jhossepmartinez <jhossepmv@gmail.com>
2025-10-08 23:31:07 -0300

    lol

--- nvim/lua/plugins/colorschemes.lua
+++ nvim/lua/plugins/colorschemes.lua
@@ -52,4 +52,5 @@ return {
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 	},
+	{ "pineapplegiant/spaceduck", branch = "main" },
 }
