22ec9b2ea5ca96dbed249a00344e0153654c563f
jhossepmartinez <jhossepmv@gmail.com>
2025-12-23 16:38:14 -0300

    aaa

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -15,6 +15,7 @@ alias gri="git rebase -i"
 # Utilities
 alias cl="clear"
 alias clz="find . -name '*:Zone.Identifier' -type f -delete"
+alias rmswap="rm -rf $HOME/.local/state/nvim/swap"
 
 # Python
 alias act="source ./venv/bin/activate.fish"
--- nvim/ftplugin/markdown.lua
+++ nvim/ftplugin/markdown.lua
@@ -1,2 +1,3 @@
 vim.opt_local.wrap = true
 vim.opt_local.linebreak = true
+vim.opt_local.breakindent = true
